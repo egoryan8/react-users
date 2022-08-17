5b01a250bd93dc046f21f89f0c956b4b67fc3df3
Fogel Egor <fogel_92@bk.ru>
2022-08-18 03:13:42 +0700

    feat: add searching  by name and email

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import './index.scss';
-import { Success } from './components/Success';
-import { Users } from './components/Users';
+import {Success} from './components/Success';
+import {Users} from './components/Users';
 
-// Тут список пользователей: https://reqres.in/api/users
 
 function App() {
   const [users, setUsers] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [searchValue, setSearchValue] = React.useState('');
 
   React.useEffect(() => {
-    fetch('https://reqres.in/api/users').then((res) => res.json()).then(res => {
-      setUsers(res.data)
-    })
-  })
+    fetch('https://reqres.in/api/users')
+      .then((res) => res.json())
+      .then(res => {
+        setUsers(res.data);
+        setIsLoading(false);
+      })
+      .catch(err => {
+        console.warn(err);
+        alert('Произошла ошибка при загурзке пользователей :(');
+      })
+  }, []);
 
-  return (
-    <div className="App">
-      <Users items={users}/>
+  const onChangeValue = (evt) => {
+    setSearchValue(evt.target.value);
+  }
+
+  return (<div className="App">
+      <Users items={users} isLoading={isLoading} onChangeValue={onChangeValue} searchValue={searchValue}/>
       {/* <Success /> */}
-    </div>
-  );
+    </div>);
 }
 
 export default App;
--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import { Skeleton } from './Skeleton';
-import { User } from './User';
+import {Skeleton} from './Skeleton';
+import {User} from './User';
 
-export const Users = ({ items, isLoading }) => {
+export const Users = ({items, isLoading, onChangeValue, searchValue}) => {
   return (
     <>
       <div className="search">
         <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-          <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
+          <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"/>
         </svg>
-        <input type="text" placeholder="Найти пользователя..." />
+        <input type="text" placeholder="Найти пользователя..." onChange={onChangeValue} value={searchValue}/>
       </div>
       {isLoading ? (
         <div className="skeleton-list">
-          <Skeleton />
-          <Skeleton />
-          <Skeleton />
+          <Skeleton/>
+          <Skeleton/>
+          <Skeleton/>
+          <Skeleton/>
         </div>
       ) : (
         <ul className="users-list">
           {
-            items.map(obj => <User {...obj}/>)
+            items
+              .filter(obj => {
+                const fullName = (obj.first_name + obj.last_name).toLowerCase();
+                return fullName.includes(searchValue.toLowerCase()) || obj.email.includes(searchValue.toLowerCase());
+              })
+              .map(obj => <User key={obj.id} {...obj}/>)
           }
         </ul>
       )}
